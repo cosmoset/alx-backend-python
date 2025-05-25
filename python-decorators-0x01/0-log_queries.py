1b8097e99f60a1a9ee46d413ecaaa4e21339bd67
Nebiyou T. <101356063+cosmoset@users.noreply.github.com>
2025-05-25 17:35:36 +0300

    Create 0-log_queries.py

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,28 @@
+import sqlite3
+import functools
+from datetime import datetime
+
+def log_queries(func):
+   @functools.wraps(func)
+   def wrapper(*args , **kwargs):
+       query = kwargs.get('query') if 'query' in kwargs else args[0] if args else None
+       if query:
+           print(f"[{datetime.now()}] ls"
+                 f"Executing This SQL query: {query} ")
+           return func(*args, **kwargs)
+   return wrapper
+
+
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+if __name__ == "__main__":
+    users = fetch_all_users("SELECT * FROM users")
+    print(users)
